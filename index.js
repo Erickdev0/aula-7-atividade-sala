5fe93f30aef20a7cff2b678d4ab29c0b807ff4b8
ERICK <161468901+Erickdev0@users.noreply.github.com>
2024-06-11 11:58:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const contarVogais = (str) => {
+  str = str.toLowerCase();
+  const vogais = "aeiou";
+  
+  let contagem = 0;
+  
+  for(let i = 0; i < str.length; i++) {
+    if (vogais.includes(str[i])) {
+      contagem++;
+    }
+  }
+  
+  return contagem;
+}
+
+const atualizarResultado = () => {
+  const inputString = document.getElementById('inputstring').value;
+  const resultado = contarVogais(inputString);
+  document.getElementById('result').innerText = `O número de vogais em '${inputString}' é: ${resultado}`;
+}
+
+document.getElementById('countButton').addEventListener('click', atualizarResultado);
